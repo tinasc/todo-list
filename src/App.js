10566fa7bd4c12be1fe5f7c32557aa79c9532af5
Tina <tinasylviac@gmail.com>
2020-10-01 14:43:23 +0530

    Local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React,{useState} from 'react';
+import React,{useState,  useEffect} from 'react';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import './App.css';
 
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+
 function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(todo) {
     setTodos([todo, ...todos]);
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,12 @@ function Todo({todo}) {
     return (
         <div className="todo-checkbox">
             <input type="checkbox"/>
+            <div style={{
+                color: "white",
+                textDecoration: todo.completed ? "line-through" : null
+                  }}>
                 {todo.task}
+            </div>
             <button>X</button>
         </div>
     );
